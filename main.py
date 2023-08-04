5c1bf572780b4d79f582c021d856061756979d1f
Kaewin <gerbald@protonmail.com>
2023-08-04 08:06:30 -0500

    mostly built now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import random
 
 
-
-
 def two_digit_addition():
 
     status = False
@@ -11,13 +9,14 @@ def two_digit_addition():
     number_2 = random.randint(10, 99)
 
     sum = number_1 + number_2
-    print(sum)
 
-    print(number_1, 
+    print("  ", 
+          number_1, 
           "\n", 
+          "+",
           number_2, 
           "\n", 
-          "------------")
+          "-----")
     
     val = input("Enter your answer: ")
     val = int(val)    
@@ -26,22 +25,146 @@ def two_digit_addition():
 
         if val == sum:
             print("Correct")
+            print()
             status = True
         if val != sum:
-            val = input("Try again: ")
-            val = int(val)
-
-
+            print("Try again\n")
+            print("  ", 
+                number_1, 
+                "\n", 
+                "+",
+                number_2, 
+                "\n", 
+                "-----")
+            
+            val = input("Enter your answer: ")
+            val = int(val)  
 
     return
 
 
-#def three_digit_addition():
+def three_digit_addition():
+    status = False
+
+    number_1 = random.randint(100, 999)
+    number_2 = random.randint(100, 999)
+
+    sum = number_1 + number_2
+
+    print("  ", 
+          number_1, 
+          "\n", 
+          "+",
+          number_2, 
+          "\n", 
+          "-----")
+    
+    val = input("Enter your answer: ")
+    val = int(val)    
+
+    while (status == False):
+
+        if val == sum:
+            print("Correct")
+            print()
+            status = True
+        if val != sum:
+            print("Try again\n")
+            print("  ", 
+                number_1, 
+                "\n", 
+                "+",
+                number_2, 
+                "\n", 
+                "-----")
+            
+            val = input("Enter your answer: ")
+            val = int(val)  
+
+    return
 
 
-#def two_digit_subtraction():
+def two_digit_subtraction():
+    status = False
 
-#def three_digit_subtraction():
+    number_1 = random.randint(10, 99)
+    number_2 = random.randint(10, 99)
+
+    sum = number_1 - number_2
+
+    print("  ", 
+          number_1, 
+          "\n", 
+          "+",
+          number_2, 
+          "\n", 
+          "-----")
+    
+    val = input("Enter your answer: ")
+    val = int(val)    
+
+    while (status == False):
+
+        if val == sum:
+            print("Correct")
+            print()
+            status = True
+        if val != sum:
+            print("Try again\n")
+            print("  ", 
+                number_1, 
+                "\n", 
+                "+",
+                number_2, 
+                "\n", 
+                "-----")
+            
+            val = input("Enter your answer: ")
+            val = int(val)  
+
+    return
+
+
+def three_digit_subtraction():
+
+    status = False
+
+    number_1 = random.randint(100, 999)
+    number_2 = random.randint(100, 999)
+
+    sum = number_1 - number_2
+
+    print("  ", 
+          number_1, 
+          "\n", 
+          "+",
+          number_2, 
+          "\n", 
+          "-----")
+    
+    val = input("Enter your answer: ")
+    val = int(val)    
+
+    while (status == False):
+
+        if val == sum:
+            print("Correct")
+            print()
+            status = True
+        if val != sum:
+            print("Try again\n")
+            print("  ", 
+                number_1, 
+                "\n", 
+                "+",
+                number_2, 
+                "\n", 
+                "-----")
+            
+            val = input("Enter your answer: ")
+            val = int(val)  
+
+    return
 
 
 def number_generator(base, length):
@@ -53,14 +176,50 @@ def number_generator(base, length):
 
 def main():
 
-    print("Welcome to the mental math trainer.")
-    print("What would you like to do?")
+    counter = 0
 
-    print("Two-Digit Addition\nThree-Digit Addition\nTwo-Digit Subtraction\nThree-Digit Subtraction")
+    print("\nWelcome to the mental math trainer.")
+    print("What would you like to do?\n")
 
-    two_digit_addition()
+    print("1 Two-Digit Addition\n2 Three-Digit Addition\n3 Two-Digit Subtraction\n4 Three-Digit Subtraction\n")
 
-    return  
+    selection = input("Input your selection: ")
+    selection = int(selection)
+    print()
+
+    if selection == 1:
+        print("How many practice problems would you like?")
+        iterations = int(input())
+        print()
+
+        while counter != iterations:
+            counter += 1
+            two_digit_addition()
+    if selection == 2:
+        print("How many practice problems would you like?")
+        iterations = int(input())
+        print()
+
+        while counter != iterations:
+            counter += 1
+            three_digit_addition()        
+    if selection == 3:
+        print("How many practice problems would you like?")
+        iterations = int(input())
+        print()
+
+        while counter != iterations:
+            counter += 1
+            two_digit_subtraction() 
+    if selection == 4:
+        print("How many practice problems would you like?")
+        iterations = int(input())
+        print()
+
+        while counter != iterations:
+            counter += 1
+            three_digit_subtraction() 
+
 
 if __name__ == "__main__":
     main()
